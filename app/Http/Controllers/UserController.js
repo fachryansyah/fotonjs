390443e419e75c02fa971e827cab8a5c82558c70
fahriansyah <fachryansyah123@gmail.com>
2019-09-15 11:40:16 +0700

    write sample code to use AuthServiceProvider

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,8 +1,14 @@
+const Auth = require("../../Providers/AuthServiceProvider")
 let UserModel = require("../../Model/UserModel")
 
 const UserController = {
     getUsers: async function(req, res) {
+        // get user data by ORM
         const users = await UserModel.query().orderBy("id", "desc")
+
+        // Retrieving The Authenticated User with AuthServiceProvider
+        const authenticatedUser = Auth.user(req) // the output should return user data
+
         res.json({
             "message" : "OKE!",
             "status" : 200,
