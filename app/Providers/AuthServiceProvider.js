a39df28ea9ac9d534243fa2994d9b5ee038ce403
fahriansyah <fachryansyah123@gmail.com>
2019-09-15 11:34:45 +0700

    added AuthServiceProvider for easly retrive data or manage user logged in application

---
+++ app/Providers/AuthServiceProvider.js
@@ -0,0 +1,27 @@
+const User = require("../Models/UserModel")
+
+const AuthServiceProvider = {
+    user: async function (req) {
+        const apiKey = req.headers.authorization.split(" ")[1]
+
+        let user = await User.query().findOne({ api_key: apiKey })
+
+        delete user["id"]
+        delete user["password"]
+
+        return user
+    },
+    logout: async function (req) {
+        const apiKey = req.headers.authorization.split(" ")[1]
+
+        const user = User.query().where({
+            api_key: apiKey
+        }).patch({
+            api_key: ""
+        })
+
+        return user
+    }
+}
+
+module.exports = AuthServiceProvider
