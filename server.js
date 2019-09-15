9fc737fbbc45645df8e256ab26f07938d33f7ecf
fahriansyah <fachryansyah123@gmail.com>
2019-09-15 11:47:53 +0700

    add body parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 let app = require("./app")
 let routeApi = require("./routes/api")
+let bodyParser = require("body-parser")
 
 const port = process.env.APP_PORT
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => console.log(`Backend started on port ${port}`))
